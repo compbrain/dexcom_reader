24ff3dc21a30530fabbd7cf643fafbbbe2f21907
Ben West <bewest@gmail.com>
2015-05-12 13:21:37 -0700

    author/maintainer info better, add udev rules
    @compbrain is author, while pypi allows marking myself up as maintainer.
    This also includes udev rules in the proper place so that permissions are taken
    care of.

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python
 
 from setuptools import setup, find_packages
+import platform
 
 import dexcom_reader
 def readme():
     with open("README.md") as f:
         return f.read()
 
+dataFiles = [ ]
+if platform.system( ) == 'Linux':
+  dataFiles = [
+      ('/etc/udev/rules.d', ['etc/udev/rules.d/80-dexcom.rules']),
+    ]
+
 setup(name='dexcom_reader',
     version='0.0.4', # http://semver.org/
     description='Audit, and inspect data from Dexcom G4.',
     long_description=readme(),
-    author="Medevice contributors",
-    # I'm just maintaining the package, compbrain authored it.
-    author_email="bewest+insulaudit@gmail.com",
+    author="Will Nowak",
+    # I'm just maintaining the package, @compbrain authored it.
+    # I don't have their email.
+    author_email="compbrain+dexcom_reader@gmail.com",
+    maintainer="Ben West",
+    maintainer_email="bewest+dexcom_reader@gmail.com",
     url="https://github.com/compbrain/dexcom_reader",
     packages=find_packages( ),
     install_requires = [
@@ -27,6 +37,7 @@ setup(name='dexcom_reader',
         'Topic :: Scientific/Engineering',
         'Topic :: Software Development :: Libraries'
     ],
+    data_files=dataFiles,
     zip_safe=False,
     include_package_data=True
 )
